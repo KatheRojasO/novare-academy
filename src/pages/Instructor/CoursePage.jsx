463a002c62f027b4b6449c349d5312b0e5623d42
KatheRojasO <kathe.rojaso@gmail.com>
2023-03-22 15:58:26 +0100

    Course page shows the proper files

--- src/pages/Instructor/CoursePage.jsx
+++ src/pages/Instructor/CoursePage.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useCourses } from "../../state/CoursesContextProvider";
+import InstructorFooter from "../../components/InstructorFooter";
+import logo from "../../assets/images/logo.png";
+import file from "../../assets/icons/file.svg";
+
 
 export default function CoursePage() {
+  const { courses } = useCourses();
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const courseInfo = courses.find((course) => course.id === params.id)
+
   return (
-    <div>CoursePage</div>
-  )
+    <div className="course-page">
+      <div className="container">
+        <div className="logo-container">
+          <img src={logo} alt="novareacademy-logo" className="novare-logo" />
+        </div>
+        <div className="sections-container">
+          <div className="sections">
+            <img src={file} alt="file-img" />
+            <h2>Recorded Classes</h2>
+          </div>
+          <div className="sections" onClick={()=> navigate(`/course-page/${courseInfo.id}/files`)}>
+            <img src={file} alt="file-img" />
+            <h2>Files</h2>
+          </div>
+          <div className="sections">
+            <img src={file} alt="file-img" />
+            <h2>Links</h2>
+          </div>
+          <div className="sections">
+            <img src={file} alt="file-img" />
+            <h2>Students</h2>
+          </div>
+        </div>
+      </div>
+
+      <InstructorFooter />
+    </div>
+  );
 }
