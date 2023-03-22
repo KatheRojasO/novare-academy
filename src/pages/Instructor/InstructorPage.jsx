86f36d1b7941818eb3c8fe2cb7ced6553d0f6522
KatheRojasO <kathe.rojaso@gmail.com>
2023-03-22 15:57:47 +0100

    Instrcutor page redirects to proper course

--- src/pages/Instructor/InstructorPage.jsx
+++ src/pages/Instructor/InstructorPage.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { readDocuments } from "../../scripts/firestore";
 import { useCourses } from "../../state/CoursesContextProvider";
 import InstructorFooter from "../../components/InstructorFooter";
 import logo from "../../assets/images/logo.png";
@@ -10,18 +9,21 @@ export default function InstructorPage() {
   const navigate = useNavigate();
 
   const items = courses.map((item) => (
-    <div className="course-card" key={item.id} onClick={()=> navigate("/course-page")}>
+    <div
+      className="course-card" key={item.id}
+      onClick={() => navigate(`/course-page/${item.id}`)}
+    >
       <h2>{item.name}</h2>
       <p>{item.description}</p>
       <p>{item.instructor}</p>
     </div>
+    
   ));
-  console.log(courses);
 
   return (
     <div className="instructor-page">
       <div className="container">
-        <img src={logo} alt="novare-logo" />
+        <img src={logo} alt="novare-logo" className="novare-logo" />
         {items}
       </div>
       <InstructorFooter />
