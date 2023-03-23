463c6daf39d0769753c74593e2621ee082f2a097
KatheRojasO <kathe.rojaso@gmail.com>
2023-03-23 21:21:51 +0100

    Fix css for add course page for every device

--- src/components/AddCourseForm.jsx
+++ src/components/AddCourseForm.jsx
@@ -21,47 +21,45 @@ export default function AddCourseForm() {
       files: [],
       links: [],
       recordings: [],
-      students: []
+      students: [],
     };
 
-    createCourse(courseObject)
-    dispatch({type: "create", payload: courseObject})
+    createCourse(courseObject);
+    dispatch({ type: "create", payload: courseObject });
 
     setName("");
     setDescription("");
-    
+
     setFormSubmit(true);
     event.target.reset();
   }
   return (
-    <div>
-      <form className="add-form" onSubmit={(event) => onSubmit(event)}>
-        <div className="form-title">
-          <h2>Add a new course:</h2>
-        </div>
-        <div className="message">
-          <p>{formSubmit && "The product was created succesfully "}</p>
-        </div>
-        <div className="form-inputs">
-          <label className="add-form-label">
-            Course name:
-            <input
-              type="text"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-          </label>
-          <label className="add-form-label">
-            Course description:
-            <input
-              type="text"
-              value={description}
-              onChange={(event) => setDescription(event.target.value)}
-            />
-          </label>
-        </div>
-          <button className="add-button">Add course</button>
-      </form>
-    </div>
+    <form className="add-form" onSubmit={(event) => onSubmit(event)}>
+      <div className="form-title">
+        <h2>Add a new course:</h2>
+      </div>
+      <div className="message">
+        <p>{formSubmit && "The product was created succesfully "}</p>
+      </div>
+      <div className="form-inputs">
+        <label className="add-form-label">
+          Course name:
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <label className="add-form-label">
+          Course description:
+          <input
+            type="text"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </label>
+      </div>
+      <button className="add-button">Add course</button>
+    </form>
   );
 }
--- src/pages/Instructor/AddCourse.jsx
+++ src/pages/Instructor/AddCourse.jsx
@@ -4,15 +4,15 @@ import AddCourseForm from "../../components/AddCourseForm";
 import logo from "../../assets/images/logo.png";
 
 export default function AddCourse() {
-  
-
   return (
     <div className="add-course">
-      <div className="container">
-        <img src={logo} alt="novare-logo" />
-        <AddCourseForm/>
+      <div className="logo-container">
+        <img src={logo} alt="novare-logo" className="novare-logo" />
       </div>
-      <InstructorNavbar/>
+      <div className="container">
+        <AddCourseForm />
+      </div>
+      <InstructorNavbar />
     </div>
   );
 }
