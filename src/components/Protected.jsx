47ca58a47238019e272ac21556686ed5e7644bce
KatheRojasO <kathe.rojaso@gmail.com>
2023-03-23 23:21:13 +0100

    Improve format of helper routes

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -5,10 +5,7 @@ import { useUser } from "../state/UserContextProvider";
 import React from "react";
 
 export default function Protected() {
-    const { user } = useUser()
+  const { user } = useUser();
 
-  // Based on https://blog.openreplay.com/role-based-access-in-react/
-    return user ?
-            ( <Outlet/> )
-            : (<Navigate to="/" /> )
-};
+  return user ? <Outlet /> : <Navigate to="/" />;
+}
--- src/components/Unauthenticated.jsx
+++ src/components/Unauthenticated.jsx
@@ -4,16 +4,16 @@ import { useUser } from "../state/UserContextProvider";
 
 import React from "react";
 
-
 export default function Unauthenticated() {
-  const { user } = useUser()
+  const { user } = useUser();
 
-// Based on https://blog.openreplay.com/role-based-access-in-react/
-  return user ?
-          (
-            user.isInstructor ?
-            (<Navigate to="/instructor-page" /> )
-            : (<Navigate to="/student-page" /> )
-          )
-          : ( <Outlet/> )
-};
+  return user ? (
+    user.isInstructor ? (
+      <Navigate to="/instructor-page" />
+    ) : (
+      <Navigate to="/student-page" />
+    )
+  ) : (
+    <Outlet />
+  );
+}
