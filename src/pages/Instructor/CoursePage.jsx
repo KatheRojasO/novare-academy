76fcb78443ea5229d180c2fa24c4f0ee3fda3115
KatheRojasO <kathe.rojaso@gmail.com>
2023-03-23 21:46:28 +0100

    Fix css for course page for every device

--- src/pages/Instructor/CoursePage.jsx
+++ src/pages/Instructor/CoursePage.jsx
@@ -8,34 +8,47 @@ import videoIcon from "../../assets/icons/video.svg";
 import logo from "../../assets/images/logo.png";
 import fileIcon from "../../assets/icons/file.svg";
 
-
 export default function CoursePage() {
   const { courses } = useCourses();
   const params = useParams();
   const navigate = useNavigate();
 
-  const courseInfo = courses.find((course) => course.id === params.id)
+  const courseInfo = courses.find((course) => course.id === params.id);
 
   return (
     <div className="course-page">
+      <div className="logo-container">
+        <img src={logo} alt="novareacademy-logo" className="novare-logo" />
+      </div>
       <div className="container">
-        <div className="logo-container">
-          <img src={logo} alt="novareacademy-logo" className="novare-logo" />
-        </div>
         <div className="sections-container">
-          <div className="sections" onClick={()=> navigate(`/course-page/${courseInfo.id}/recordings`)} >
+          <div
+            className="sections"
+            onClick={() => navigate(`/course-page/${courseInfo.id}/recordings`)}
+          >
             <img src={videoIcon} alt="file-img" />
             <h2>Recorded Classes</h2>
           </div>
-          <div className="sections" onClick={()=> navigate(`/course-page/${courseInfo.id}/files`)}>
+          <div
+            className="sections"
+            onClick={() => navigate(`/course-page/${courseInfo.id}/files`)}
+          >
             <img src={fileIcon} alt="file-img" />
             <h2>Files</h2>
           </div>
-          <div className="sections" onClick={()=> navigate(`/course-page/${courseInfo.id}/links`)}>
+          <div
+            className="sections"
+            onClick={() => navigate(`/course-page/${courseInfo.id}/links`)}
+          >
             <img src={linkIcon} alt="file-img" />
             <h2>Links</h2>
           </div>
-          <div className="sections" onClick={()=> navigate(`/course-page/${courseInfo.id}/student-list`)}>
+          <div
+            className="sections"
+            onClick={() =>
+              navigate(`/course-page/${courseInfo.id}/student-list`)
+            }
+          >
             <img src={userIcon} alt="file-img" />
             <h2>Students</h2>
           </div>
